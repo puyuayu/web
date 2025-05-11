9ad9449706e97631f64f946e2a9f4ed35d3c1b86
Purwati Ayu Utami <152758926+puyuayu@users.noreply.github.com>
2025-05-11 23:50:46 +0700

    TUGAS Form Login

--- Pertemuan11/from_login/index.php
+++ Pertemuan11/from_login/index.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (isset($_SESSION['user'])) {
+    header("Location: dashboard.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- Pertemuan11/from_login/login.php
+++ Pertemuan11/from_login/login.php
@@ -7,9 +7,10 @@ $query = "SELECT * FROM users WHERE username='$username'";
 $result = mysqli_query($conn, $query);
 $user = mysqli_fetch_assoc($result);
 if ($user && password_verify($password, $user['password'])) {
-$_SESSION['user'] = $user['username'];
-header("Location: dashboard.php");
+    $_SESSION['user'] = $user['username'];
+    header("Location: dashboard.php");
 } else {
-echo "Login gagal. Username atau password salah.";
+    header("Location: index.php?error=1");
+    exit;
 }
 ?>
